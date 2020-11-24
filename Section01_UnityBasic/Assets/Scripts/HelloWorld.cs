fbde801f7c60fc1d3dde2f158522ad9a5d1f2f11
chicken-developer <quynh.fullstackdev@gmail.com>
2020-11-24 09:52:15 +0700

    [Quynh] Config Unity3D with Visual Studio Professional 2019
    Notes: Can use Rider or VSCode, Atom, Vim,...

---
+++ Section01_UnityBasic/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hello from Visual studio");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log("This line print from car object every frame");
+    }
+}
